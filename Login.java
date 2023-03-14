98e31eb2fdafce87c091bb4c6f05e5864af88137
waridhasan195 <waridhasan195@gmail.com>
2023-03-14 20:05:30 +0600

    First Commit

---
+++ Login.java
@@ -0,0 +1,18 @@
+package Bank_Management;
+
+import javax.swing.JFrame;
+
+public class Login extends JFrame{
+
+    Login(){
+        setSize(800, 400);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        new Login();
+        
+    }
+    
+}
