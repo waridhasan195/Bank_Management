4cc234e2595ca4de0279d4768df24b689367efa5
waridhasan195 <waridhasan195@gmail.com>
2023-03-31 01:02:51 +0600

    Third

--- SignupThree.java
+++ SignupThree.java
@@ -1,9 +1,13 @@
 package Bank_Management;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.smartcardio.Card;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
@@ -56,12 +60,105 @@ public class SignupThree extends JFrame {
         Account_Type_Group.add(Fixed_acc);
         Account_Type_Group.add(Recurring_acc);
 
+        JLabel Card_Text = new JLabel("Card Number: ");
+        Card_Text.setFont(new Font("Raleway", Font.BOLD, 20));
+        Card_Text.setBounds(100, 250, 200, 40);
+        add(Card_Text);
+
+        JLabel Card_Number = new JLabel("XXXX-XXXX-XXXX-4168");
+        Card_Number.setFont(new Font("Raleway", Font.BOLD, 15));
+        Card_Number.setBounds(280, 250, 250, 40);
+        add(Card_Number);
+
+        JLabel Card_Number_Hints = new JLabel("Your 16 Digits Card Number");
+        Card_Number_Hints.setFont(new Font("Raleway", Font.PLAIN, 12));
+        Card_Number_Hints.setBounds(280, 267, 250, 40);
+        add(Card_Number_Hints);
+
+
+        JLabel PIN_Text = new JLabel("PIN Number: ");
+        PIN_Text.setFont(new Font("Raleway", Font.BOLD, 20));
+        PIN_Text.setBounds(100, 340, 200, 40);
+        add(PIN_Text);
+
+        JLabel PIN_Number = new JLabel("XXXX");
+        PIN_Number.setFont(new Font("Raleway", Font.BOLD, 15));
+        PIN_Number.setBounds(280, 340, 250, 40);
+        add(PIN_Number);
+
+        JLabel PIN_Number_Hints = new JLabel("Your 4 Digits Password");
+        PIN_Number_Hints.setFont(new Font("Raleway", Font.PLAIN, 12));
+        PIN_Number_Hints.setBounds(280, 357, 250, 40);
+        add(PIN_Number_Hints);
+
+
+        JLabel Service_Text = new JLabel("Service Required");
+        Service_Text.setFont(new Font("Raleway", Font.BOLD, 20));
+        Service_Text.setBounds(100, 430, 200, 40);
+        add(Service_Text);
+
+        JCheckBox ATM_Card = new JCheckBox("ATM CARD");
+        ATM_Card.setBackground(Color.WHITE);
+        ATM_Card.setBounds(150, 480, 100, 40);
+        add(ATM_Card);
+
+        JCheckBox Internet_Banking = new JCheckBox("Internet Banking");
+        Internet_Banking.setBackground(Color.WHITE);
+        Internet_Banking.setBounds(400, 480, 130, 40);
+        add(Internet_Banking);
+
+        JCheckBox Mobile_Banking = new JCheckBox("Mobile Banking");
+        Mobile_Banking.setBackground(Color.WHITE);
+        Mobile_Banking.setBounds(150, 520, 130, 40);
+        add(Mobile_Banking);
+
+        JCheckBox Email_SMS = new JCheckBox("EMAIL & SMS Alerts");
+        Email_SMS.setBackground(Color.WHITE);
+        Email_SMS.setBounds(400, 520, 160, 40);
+        add(Email_SMS);
+
+        JCheckBox Check_Book = new JCheckBox("Check Book");
+        Check_Book.setBackground(Color.WHITE);
+        Check_Book.setBounds(150, 560, 100, 40);
+        add(Check_Book);
+
+        JCheckBox E_Statement = new JCheckBox("E - Statement");
+        E_Statement.setBackground(Color.WHITE);
+        E_Statement.setBounds(400, 560, 130, 40);
+        add(E_Statement);
+
+        JCheckBox Aggrement_Check = new JCheckBox("I have declares that above entered details are correct to the best of my knowledge.");
+        Aggrement_Check.setFont(new Font("raleway", Font.BOLD, 14));
+        Aggrement_Check.setBounds(100, 620, 700, 40);
+        Aggrement_Check.setBackground(Color.WHITE);
+        add(Aggrement_Check);
+
+
+
+        JButton Cancel = new JButton("Cancel");
+        Cancel.setBackground(Color.BLACK);
+        Cancel.setForeground(Color.WHITE);
+        Cancel.setBounds(100, 690, 100, 30);
+        add(Cancel);
+
+        JButton Submit = new JButton("SUBMIT");
+        Submit.setBackground(new Color(0, 225, 190));
+        Submit.setForeground(Color.black);
+        Submit.setBounds(230, 690, 100, 30);
+        add(Submit);
+
+
+
+
+
+
         getContentPane().setBackground(Color.WHITE);
-        setSize(800, 800);
+        setSize(800, 900);
         setVisible(true);
 
 
     }
+
 
 
     public static void main(String[] args) {
